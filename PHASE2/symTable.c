935a72b96a812a49fc282c5ce976a4d458264c75
Manos Lourakis <csd3153@csd.uoc.gr>
2020-03-21 09:56:09 +0200

    Added revised symTable library implementation

---
+++ PHASE2/symTable.c
@@ -0,0 +1,5 @@
+#include "symTable.h";
+
+int hashFunc(char* name){
+    //ADD CODE
+}
---
+++ PHASE2/symTable.h
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+#define BUCKETS 512
+
+enum SymbolType {GLOBAL, LOCAL, FORMAL, USERFUNC, LIBFUNC};
+
+typedef struct Variable {
+    const char* name;
+    unsigned int scope;
+    unsigned int line;
+};
+
+typedef struct Function {
+    const char* name;
+    // List of argumenst - TODO!!
+    unsigned int scope;
+    unsigned int line;
+};
+
+// A list of the symbols saved in each scope.
+typedef struct ScopeListNode {
+    unsigned int scope;
+    symbolTableEntry *symbol_list;
+
+    ScopeListNode *next;
+};
+
+typedef struct SymbolTableEntry {
+    bool isActive;
+    union{
+        Variable *varVal;
+        Function *funcVal;
+    } value;
+    enum SymbolType type;
+
+    SymbolTableEntry *next; //  Points to the next symbol in the hash list. 
+    ScopeListNode *scope_next; // Points to the next symbol in scope list
+};
+
+ScopeListNode *scopeListHead = NULL; // Global pointer to scope list's head.
+SymbolTableEntry *SymbolTable[BUCKETS];
+    
+int hashFunc(char* name);
+
+void insert(SymbolTableEntry symbol);
+
+int scopeLookup(int scope);
+
+int totalLookup(int scope);
+
+void hide();
+
+char* generateFuncName();
+
+void initLibFuncs();
+
+void printScopeList();
