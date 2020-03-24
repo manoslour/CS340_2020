9c3f54b28ba2e24c311e9e4224d7f6e80c1bbc4e
Manos Lourakis <csd3153@csd.uoc.gr>
2020-03-24 19:36:53 +0200

    Added Loukas files

--- PHASE2/symTable.h
+++ PHASE2/symTable.h
@@ -2,64 +2,73 @@
 #define _SYMTABLE_H_
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
-#define BUCKETS 512
+#define RED   "\x1B[31m"
+#define GRN   "\x1B[32m"
+#define YEL   "\x1B[33m"
+#define BLU   "\x1B[34m"
+#define RESET "\x1B[0m"
 
-enum SymbolType {GLOBAL, LOCAL, FORMAL, USERFUNC, LIBFUNC};
+#define Buckets 256
 
-typedef struct Variable {
-    const char* name;
-    unsigned int scope;
-    unsigned int line;
+enum SymbolType {Global, Local, Formal, Userfunc, Libfunc}; 
+
+typedef struct Variable{
+	const char *name;
+	unsigned int scope;
+	unsigned int line ;
 }Variable;
 
-typedef struct Function {
-    const char* name;
-    // List of argumenst - TODO!!
-    unsigned int scope;
-    unsigned int line;
+typedef struct Function{
+	const char *name; 
+	unsigned int scope ;
+	unsigned int line;
+	struct SymbolTableEntry *next;
 }Function;
 
-typedef struct SymbolTableEntry {
-    bool isActive;
-    union{
-        Variable *varVal;
-        Function *funcVal;
-    } value;
-    enum SymbolType type;
+typedef struct SymbolTableEntry{
+	bool isActive;
+	union {
+		Variable *varVal;
+		Function *funcVal;
+	} value;
+	enum SymbolType type;
 
-    struct SymbolTableEntry *next; //  Points to the next symbol in the hash list. 
-    struct ScopeListNode *scope_next; // Points to the next symbol in scope list
-
+	struct SymbolTableEntry *next, *scope_next, *formal_next; 
 }SymbolTableEntry;
 
-// Used to create a list of the symbols saved in each scope.
 typedef struct ScopeListEntry{
-    unsigned int scope;
-    SymbolTableEntry *symbol_list;
-
-    struct ScopeListEntry *next;
+	unsigned int scope;
+	struct SymbolTableEntry *symbols;
+	struct ScopeListEntry *next, *prev;
 }ScopeListEntry;
 
+ScopeListEntry *scope_head = NULL; //Global pointer to the scope list's head
+SymbolTableEntry *HashTable[Buckets];
 
-ScopeListEntry *scopeListHead = NULL; // Global pointer to scope list's head.
-SymbolTableEntry *SymbolTable[BUCKETS];
-    
-int hashFunc(char* name);
-
-void insert(SymbolTableEntry *symbol);
-
-int scopeLookup(unsigned int scope);
-
-int totalLookup(unsigned int scope);
-
-void hide(unsigned int scope);
-
-char* generateFuncName();
-
-void initLibFuncs();
+void printHash();
 
 void printScopeList();
+ 
+bool hide (int scope );
+
+bool enable (int scope );
+
+bool hashInsert(char *name, unsigned int line, enum SymbolType type, unsigned int scope);
+
+bool scopeListInsert (struct SymbolTableEntry *sym_node, unsigned int scope);
+
+void initialize();
+
+bool scopeLookUp(char *name, unsigned int scope);
+
+bool generalLookUp(char *name);
+
+void hideScope(unsigned int scope);
+
+void activateScope(unsigned int scope);
 
 #endif
